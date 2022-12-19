059491129fd0be8be5ad7a008259545930b93e5f
abdulsagheer <abdulsagheeras29@gmail.com>
2022-12-19 23:03:59 +0530

    Setting up basic folder structure for the project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function App() {
+  return <Canvas></Canvas>;
+}
+
+export default App;

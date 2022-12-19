50448c13ab807fcf8a6ebaeba225d4d3a7c18bc8
abdulsagheer <abdulsagheeras29@gmail.com>
2022-12-19 23:20:18 +0530

    duplicated the mesh

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import "./App.css";
 import { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 
+export const Cube = (props) => {
+  const mesh = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame(() => (mesh.current.rotation.x += 0.01));
+  return (
+    <mesh
+      ref={mesh}
+      onPointerOver={(event) => setHovered(true)}
+      onPointerOut={(event) => setHovered(false)}
+      onClick={(event) => setActive(!active)}
+      scale={active ? 1.5 : 1}
+      {...props}
+    >
+      <boxBufferGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color={hovered ? "red" : "yellow"} />
+    </mesh>
+  );
+};
+
 function App() {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Cube position={[-1.2, 0, 0]} />
+      <Cube position={[1.2, 0, 0]} />
+    </Canvas>
+  );
 }
 
 export default App;
